eda65bc8725ca7178ebba1b922735603aa449bf4
taylor <258266864@qq.com>
2020-04-22 16:20:47 +0800

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Weibo-Laravel
                 </div>
 
                 <div class="links">
